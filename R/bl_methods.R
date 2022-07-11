5ffe89999265bafc712289cc1b175b5d31de69d7
Lachlan Astfalck <astfalckl@gmail.com>
2022-07-11 18:10:26 +0800

    Defined belief structure and set up readme

--- R/bl_methods.R
+++ R/bl_methods.R
@@ -11,40 +11,57 @@
 
 # ------------------------------------------------------------------------------
 
-#' Crate a Bayes linear object
+#' Crate a belief structure
 #'
-#' @param exp_X Prior expectation of X
-#' @param exp_D Prior expectation of D
+#' @param E_X Prior expectation of X
+#' @param E_D Prior expectation of D
 #' @param cov_XD Prior covariance of X and D
 #' @param var_X Prior variance of X
 #' @param var_D Prior variance of D
 #'
-#' @return Returns a bl object
+#' @return Returns a belief structure (bs) object
 #' @export
-bl <- function(exp_X, exp_D, cov_XD, var_X, var_D){
+bs <- function(E_X, E_D, cov_XD, var_X, var_D){
 
-  new_bl_output(exp_X, exp_D, cov_XD, var_X, var_D)
+  new_bs_output(E_X, E_D, cov_XD, var_X, var_D)
 
 }
 
 #' @export
-new_bl_output <- function(exp_X, exp_D, cov_XD, var_X, var_D){
+new_bs_output <- function(E_X, E_D, cov_XD, var_X, var_D){
   structure(
     list(
-			exp_X = as.matrix(exp_X),
-			exp_D = as.matrix(exp_D),
+			E_X = as.matrix(E_X),
+			E_D = as.matrix(E_D),
 			cov_XD = as.matrix(cov_XD),
 			var_X = as.matrix(var_X),
 			var_D = as.matrix(var_D),
-			n_X = nrow(exp_X),
-			n_D = nrow(exp_D)
+			n_X = nrow(E_X),
+			n_D = nrow(E_D)
 		),
-    class = "bl"
+    class = "bs"
   )
 }
 
 #' @export
-print.bl <- function(object){
+new_adj_bs_output <- function(E_adj, E_D, cov_adj, var_adj, var_D, D){
+  structure(
+    list(
+			E_adj = as.matrix(E_adj),
+			E_D = as.matrix(E_D),
+			cov_adj = as.matrix(cov_adj),
+			var_adj = as.matrix(var_adj),
+			var_D = as.matrix(var_D),
+			D = list(D),
+			n_X = nrow(E_adj),
+			n_D = nrow(E_D)
+		),
+    class = "adj_bs"
+  )
+}
+
+#' @export
+print.bs <- function(object){
   utils::str(object)
 }
 
@@ -53,36 +70,37 @@ adjust <- function(obj, ...) {
   UseMethod("adjust")
 }
 
-#' Adjust a bl object with observed data
+#' Adjust a belief structure with observed data
 #'
-#' @param bl_obj A bl object
+#' @param bs_obj A belief structure
 #' @param D A matrix of observed data
 #'
-#' @return Returns a bl_adjust object
+#' @return Returns a adjusted belief structure (adj_bs) object
 #' @export
-adjust.bl <- function(bl_obj, D){
+adjust.bs <- function(bs_obj, D){
 
 	D <- as.matrix(D)
 
-	adj_exp <- bl_obj$exp_X + bl_obj$cov_XD %*% inv(bl_obj$var_D) %*% 
-		(D - bl_obj$exp_D)
-	adj_var <- bl_obj$var_X - bl_obj$cov_XD %*% inv(bl_obj$var_D) %*% 
-		t(bl_obj$cov_XD)
+	E_adj <- bs_obj$E_X + bs_obj$cov_XD %*% inv(bs_obj$var_D) %*% 
+		(D - bs_obj$E_D)
+	var_adj <- bs_obj$var_X - bs_obj$cov_XD %*% inv(bs_obj$var_D) %*% 
+		t(bs_obj$cov_XD)
 
-	bl_obj$D <- D
-	bl_obj$adjusted_exp <- adj_exp
-	bl_obj$adjusted_var <- adj_var
-	bl_obj$resolved_var <- bl_obj$var_X - adj_var
+	bs_obj$D <- D
+	bs_obj$adj_exp <- E_adj
+	bs_obj$adj_var <- var_adj
+	# ADJUSTED COVARIANCE
+	bs_obj$res_var <- bs_obj$var_X - var_adj
 
-	if (bl_obj$n_X == 1) {
-		bl_obj$resolution <- 1 - diag(adj_var)/diag(bl_obj$var_X)
-	} else {
-		bl_obj$resolution <- diag(1 - diag(diag(adj_var))/diag(diag(bl_obj$var_X)))	
-	}
+	# if (bs_obj$n_X == 1) {
+	# 	bs_obj$resolution <- 1 - diag(adj_var)/diag(bs_obj$var_X)
+	# } else {
+	# 	bs_obj$resolution <- diag(1 - diag(diag(adj_var))/diag(diag(bs_obj$var_X)))	
+	# }
 	
-	class(bl_obj) <- "bl_adjust"
+	class(bs_obj) <- "adj_bs"
 
-	return(bl_obj)
+	return(bs_obj)
 }
 
 
